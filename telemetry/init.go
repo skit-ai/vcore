469d38a5f77d24d43061ea6f551fc121d5f759b8
Aditya Satyavada <aditya@vernacular.ai>
2019-07-25 11:48:27 +0530

    refactor: Enable/Disable newrelic based on the ENV var

--- telemetry/init.go
+++ telemetry/init.go
@@ -1,19 +1,59 @@
 package telemetry
 
 import (
+	"net/http"
 	"os"
 
-	newrelic "github.com/newrelic/go-agent"
+	"github.com/newrelic/go-agent"
 )
+
+// Wrapper for newrelic.Application
+type NewRelicClient struct {
+	application newrelic.Application
+}
+
+// Wrapper for newrelic.Transaction
+type Transaction struct {
+	transaction newrelic.Transaction
+}
+
+func newClient() NewRelicClient{
+	appName := os.Getenv("NEW_RELIC_APP_NAME")
+	licenseKey := os.Getenv("NEW_RELIC_LICENSE_KEY")
+
+	if appName != "" && licenseKey != "" {
+		// Initialize Newrelic
+		config := newrelic.NewConfig(appName, licenseKey)
+		if application, err := newrelic.NewApplication(config); err == nil {
+			// Only initialize the newrelic client if there is no error
+			return NewRelicClient{application}
+		} else {
+			return NewRelicClient{nil}
+		}
+	} else {
+		return NewRelicClient{nil}
+	}
+}
 
 var (
-	//NewrelicClient
-	NewrelicClient newrelic.Application
+	//Global Newrelic Client
+	NewrelicClient = newClient()
 )
 
-func Init() {
+// Wrapper to only start the transaction if the newrelic application struct was successfully instantiated
+func (c *NewRelicClient) StartTransaction(name string, w http.ResponseWriter, r *http.Request) Transaction {
+	if c.application != nil {
+		return Transaction{c.application.StartTransaction(name, w, r)}
+	} else {
+		return Transaction{nil}
+	}
+}
 
-	// Initialize Newrelic
-	config := newrelic.NewConfig(os.Getenv("NEW_RELIC_APP_NAME"), os.Getenv("NEW_RELIC_LICENSE_KEY"))
-	NewrelicClient, _ = newrelic.NewApplication(config)
+// Wrapper to end a transaction only if the newrelic transaction struct was created
+func (t *Transaction) End() error {
+	if t.transaction != nil {
+		return t.transaction.End()
+	} else {
+		return nil
+	}
 }
