644ffe6eee406c8e5f13a436973d5ce55f1d6186
Mukunth <mukunth@vernacular.ai>
2021-08-19 16:53:23 +0530

    pass sentry release to initsentry

--- surveillance/sentry.go
+++ surveillance/sentry.go
@@ -21,9 +21,11 @@ type Sentry struct {
 	handler *sentryWrapper.Handler
 }
 
-func initSentry() (client *Sentry) {
+func InitSentry(release string) (client *Sentry) {
 	dsn := os.Getenv("SENTRY_DSN")
-	release := os.Getenv("SENTRY_RELEASE")
+	if release == "" {
+		release = os.Getenv("SENTRY_RELEASE")
+	}
 	if dsn != "" {
 		if err := sentry.Init(sentry.ClientOptions{
 			Dsn: dsn,
@@ -54,7 +56,7 @@ func initSentry() (client *Sentry) {
 }
 
 var (
-	SentryClient = initSentry()
+	SentryClient = InitSentry("")
 )
 
 // Handles an error by capturing it on Sentry and logging the same on STDOUT
