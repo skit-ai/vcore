522121e698eb79d90bd5f6058b30de70891d2515
Nirabhra Tapaswi <nirabhra.tapaswi@skit.ai>
2022-06-10 15:30:07 +0530

    feat(crypto): added functions for base64 conversion in encryption and decryption

--- crypto/decrypt.go
+++ crypto/decrypt.go
@@ -1,5 +1,11 @@
 package crypto
 
+import (
+	"encoding/base64"
+
+	"github.com/Vernacular-ai/vcore/errors"
+)
+
 // Decrypt a byte array
 //
 // This function accepts an incoming byte array, decrypts it using AES-256 decryption and returns the result in bytes
@@ -26,16 +32,52 @@ func DecryptBytes(cipherData []byte) []byte {
 
 // Decrypt a byte array
 //
-// This function accepts an incoming byte array, decrypts it using AES-256 decryption, converts the result into a string and returns the string
+// This function accepts an incoming byte array, decrypts it using AES-256 decryption,
+// converts the result into a string and returns the string
 func DecryptString(data []byte) string {
-	/**
-	**validate logic**
-	Decrypt incoming string
-	*/
+	// Decrypt bytes, convert to string and return
+	return string(DecryptBytes(data))
+}
+
+// Decrypt a base64-encoded encrypted string to unencrypted string
+//
+// This function accepts an incoming base64 encoded string, base64 decodes it,
+// decrypts it using EncryptBytes func, converts the result into a string and returns resultant string.
+// Note: Only use when string data was encrypted.
+func DecryptB64ToString(data string) (decrypted_string string, err error) {
+	// Convert incoming string to bytes
+	byte_data, err := base64.StdEncoding.DecodeString(data)
+	if err != nil {
+		err = errors.NewError("Failed to base64-decode incoming string, please check if "+
+			"base64 encoded string is supplied", err, false)
+		return
+	}
+
+	// Decrypt bytes
+	decrypted_data := DecryptBytes(byte_data)
+
+	// Convert to string
+	decrypted_string = string(decrypted_data)
+
+	return
+}
+
+// Decrypt a base64-encoded encrypted string to unencrypted bytes
+//
+// This function accepts an incoming base64 encoded string, base64 decodes it,
+// decrypts it using EncryptBytes func and returns resultant byte array.
+func DecryptB64ToBytes(data string) (decrypted_data []byte, err error) {
 
 	// Convert incoming string to bytes
-	var byte_data = []byte(data)
+	byte_data, err := base64.StdEncoding.DecodeString(data)
+	if err != nil {
+		err = errors.NewError("Failed to base64-decode incoming string, please check if "+
+			"base64 encoded string is supplied", err, false)
+		return
+	}
 
-	// Decrypt bytes, convert to string and return
-	return string(DecryptBytes(byte_data))
+	// Decrypt bytes
+	decrypted_data = DecryptBytes(byte_data)
+
+	return
 }
