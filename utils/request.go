13350b9f138c45094c77daf86ff012efaa10f9ba
Avijeet Diwaker <30440778+avvijeet@users.noreply.github.com>
2022-06-17 17:05:29 +0530

    added func to HTTPGetWithCACrt

--- utils/request.go
+++ utils/request.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"io/ioutil"
 	"net/http"
 )
@@ -13,6 +15,41 @@ func HTTPGet(url string) (payloadBytes []byte, err error) {
 	}
 	// Download from URL
 	resp, err := httpClient.Do(req)
+	if err != nil || resp.StatusCode != http.StatusOK {
+		payloadBytes = nil
+		return
+	}
+	defer resp.Body.Close()
+
+	payloadBytes, err = ioutil.ReadAll(resp.Body)
+	return
+}
+
+// HTTPGetWithCACrt - Download from url to a bytes array using CA crt
+// pathToCaCertFile = Mount path of the CAcrt file
+func HTTPGetWithCACrt(pathToCaCertFile string, url string) (payloadBytes []byte, err error) {
+	caCert, err := ioutil.ReadFile(pathToCaCertFile)
+	if err != nil {
+		return
+	}
+
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(caCert)
+
+	client := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				RootCAs: caCertPool,
+			},
+		},
+	}
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return
+	}
+	// Download from URL
+	resp, err := client.Do(req)
 	if err != nil || resp.StatusCode != http.StatusOK {
 		payloadBytes = nil
 		return
